373d3dc8ab1d425168e500e77c5a903cc7ff2414
nomura <angelan@mihoshi.info>
2015-07-16 13:49:35 +0900

    first

---
+++ hashValidator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 423
+ * Date: 2015/07/16
+ * Time: 10:37
+ */
+
+namespace mihoshi\hashValidator;
+
+
+class hashValidator
+{
+
+}
---
+++ hashValidatorTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 423
+ * Date: 2015/07/16
+ * Time: 10:37
+ */
+
+namespace mihoshi\hashValidator;
+
+
+class hashValidatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreateInstance(){
+
+        $this->assertTrue(true);
+    }
+}
