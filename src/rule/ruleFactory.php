d26a370ddd9d25c3d600f30a98641c58b84aaa42
nomura mitsuru <nomura@success-corp.co.jp>
2017-01-24 19:56:46 +0900

    ruleのロードを一回に(再

--- src/rule/ruleFactory.php
+++ src/rule/ruleFactory.php
@@ -12,45 +12,51 @@ use mihoshi\hashValidator\exceptions\invalidRuleException;
 
 final class ruleFactory
 {
-	/**
-	 * @var array [ルール => クラス名] ロード済みのルール
-	 */
-	private static $typeList = [];
+    /**
+     * @var array [ルール => クラス名] ロード済みのルール
+     */
+    private static $typeList = [];
 
-	/**
-	 * ルールクラスの作成
-	 * @param array $rule
-	 * @return mixed
-	 */
-	public static function getInstance(array $rule)
-	{
-		try {
-			$className = self::getClassName($rule['type']);
-			unset($rule['type']);
-			return new $className($rule);
-		} catch (invalidRuleException $e) {
-			throw $e;
-		} catch (\Exception $e) {
-			throw new invalidRuleException($e->getMessage(), $e->getCode(), $e);
-		}
-	}
+    /**
+     * ルールクラスの作成
+     * @param array $rule
+     * @return mixed
+     */
+    public static function getInstance(array $rule)
+    {
+        try {
+            $className = self::getClassName($rule['type']);
+            unset($rule['type']);
+            return new $className($rule);
+        } catch (invalidRuleException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            throw new invalidRuleException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
 
-	/**
-	 * ルールクラス名の取得
-	 * @param $rule
-	 * @return string
-	 */
+    /**
+     * ルールクラス名の取得
+     * @param $rule
+     * @return string
+     */
     private static function getClassName($rule)
     {
-        if (array_key_exists($rule, self::$typeList)) {
-            return self::$typeList[$rule];
-        }
-        if (class_exists(__NAMESPACE__ . '\\' . $rule . 'Rule')) {
-            self::$typeList[$rule] = __NAMESPACE__ . '\\' . $rule . 'Rule';
-        } elseif (class_exists($rule)) {
-            self::$typeList[$rule] = $rule;
-        } else {
-            throw new invalidRuleException('rule not found:' . $rule);
+        try {
+            if (array_key_exists($rule, self::$typeList)) {
+                return self::$typeList[$rule];
+            }
+            if (class_exists(__NAMESPACE__ . '\\' . $rule . 'Rule')) {
+                self::$typeList[$rule] = __NAMESPACE__ . '\\' . $rule . 'Rule';
+            } elseif (class_exists($rule)) {
+                self::$typeList[$rule] = $rule;
+            } else {
+                throw new invalidRuleException('rule not found:' . $rule);
+            }
+        } catch (invalidRuleException $e) {
+            throw $e;
+        } catch (\Exception $e) {
+            throw new invalidRuleException($e->getMessage(), $e->getCode(), $e);
         }
         return self::$typeList[$rule];
     }
